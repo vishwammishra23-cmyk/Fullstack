722a2da7c4a854255807753f857d198f143b433a
vishwammishra23-cmyk <vishwammishra23@gmail.com>
2026-04-23 14:43:59 +0530

    Delete backend directory

--- backend/src/main/java/com/example/jobportal/repository/ApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jobportal.repository;
-
-import com.example.jobportal.model.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
-}
--- backend/src/main/java/com/example/jobportal/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jobportal.repository;
-
-import com.example.jobportal.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
